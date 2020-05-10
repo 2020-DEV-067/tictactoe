d04e262c70644d34b5a147c239951c39087ada99
tictacdev <email@abc.com>
2020-05-10 18:30:12 +0200

    Rearranging methods and restricting visibility on few methods

--- src/main/java/com/game/tictactoe/service/GameService.java
+++ src/main/java/com/game/tictactoe/service/GameService.java
@@ -30,22 +30,7 @@ public class GameService {
         }
     }
 
-    private void evaluateNewState(PlayerSymbol playerSymbol, Position position) {
-        String message;
-        if (hasCurrentPlayerWon(playerSymbol, position)) {
-            isGameActive = false;
-            message = String.format(WIN_MESSAGE, playerSymbol);
-        } else if (isBoardFull()) {
-            isGameActive = false;
-            message = DRAW_MESSAGE;
-        } else {
-            currentPlayerSymbol = getNextPlayerSymbol();
-            message = String.format(NEXT_MESSAGE, currentPlayerSymbol);
-        }
-        currentState = new GameState(board, message);
-    }
-
-    public void addSymbolToBoard(PlayerSymbol symbol, Position position) {
+    void addSymbolToBoard(PlayerSymbol symbol, Position position) {
         if (isPositionInvalid(position)) {
             throw new InvalidCoordinateException();
         }
@@ -63,20 +48,22 @@ public class GameService {
         return board.getPlayerSymbol(position) != null;
     }
 
-    private PlayerSymbol getNextPlayerSymbol() {
-        return currentPlayerSymbol == PlayerSymbol.X ? PlayerSymbol.O : PlayerSymbol.X;
-    }
-
-    public boolean isBoardFull() {
-        for (int i = 0; i < BOARD_DIMENSION; i++) {
-            for (int j = 0; j < BOARD_DIMENSION; j++) {
-                if (board.getPlayerSymbol(new Position(i, j)) == null) return false;
-            }
+    private void evaluateNewState(PlayerSymbol playerSymbol, Position position) {
+        String message;
+        if (hasCurrentPlayerWon(playerSymbol, position)) {
+            isGameActive = false;
+            message = String.format(WIN_MESSAGE, playerSymbol);
+        } else if (isBoardFull()) {
+            isGameActive = false;
+            message = DRAW_MESSAGE;
+        } else {
+            currentPlayerSymbol = getNextPlayerSymbol();
+            message = String.format(NEXT_MESSAGE, currentPlayerSymbol);
         }
-        return true;
+        currentState = new GameState(board, message);
     }
 
-    public boolean hasCurrentPlayerWon(PlayerSymbol symbol, Position lastPosition) {
+    boolean hasCurrentPlayerWon(PlayerSymbol symbol, Position lastPosition) {
         int horizontalCounter = 0, verticalCounter = 0, diagonalCounter = 0, antiDiagonalCounter = 0;
 
         for (int i = 0; i < BOARD_DIMENSION; i++) {
@@ -93,6 +80,19 @@ public class GameService {
                 || verticalCounter == BOARD_DIMENSION
                 || diagonalCounter == BOARD_DIMENSION
                 || antiDiagonalCounter == BOARD_DIMENSION;
+    }
+
+    boolean isBoardFull() {
+        for (int i = 0; i < BOARD_DIMENSION; i++) {
+            for (int j = 0; j < BOARD_DIMENSION; j++) {
+                if (board.getPlayerSymbol(new Position(i, j)) == null) return false;
+            }
+        }
+        return true;
+    }
+
+    private PlayerSymbol getNextPlayerSymbol() {
+        return currentPlayerSymbol == PlayerSymbol.X ? PlayerSymbol.O : PlayerSymbol.X;
     }
 
     public void restart() {
