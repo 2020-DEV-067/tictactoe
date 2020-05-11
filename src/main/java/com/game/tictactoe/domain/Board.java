70dded6ac7075a4f87930dd3eca782f0f45bb1ef
tictacdev <email@abc.com>
2020-05-11 09:26:45 +0200

    Removing comments from Board (method names explain what they do)

--- src/main/java/com/game/tictactoe/domain/Board.java
+++ src/main/java/com/game/tictactoe/domain/Board.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 import static com.game.tictactoe.util.GameConstant.BOARD_DIMENSION;
 
-/**
- * Represents the tic tac toe board
- */
 @Data
 public class Board {
 
@@ -16,22 +13,10 @@ public class Board {
         gameField = new PlayerSymbol[BOARD_DIMENSION][BOARD_DIMENSION];
     }
 
-    /**
-     * Adds a new player symbol to the board at a provided position
-     *
-     * @param symbol
-     * @param position
-     */
     public void addPlayerSymbol(PlayerSymbol symbol, Position position) {
         gameField[position.getX()][position.getY()] = symbol;
     }
 
-    /**
-     * Retrieves a player symbol from the board
-     *
-     * @param position
-     * @return playerSymbol
-     */
     public PlayerSymbol getPlayerSymbol(Position position) {
         return gameField[position.getX()][position.getY()];
     }
